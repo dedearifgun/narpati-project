ab7f1581a628f2c4cce51b060b08a93fe5362b0f
dedearifgun <dedearifgunawan@gmail.com>
2025-10-24 16:22:17 +0700

    feat(TokoPage): add Google Maps link for m

--- frontend/src/pages/TokoPage.js
+++ frontend/src/pages/TokoPage.js
@@ -7,7 +7,7 @@ const TokoPage = () => {
     { id: 2, name: 'Cabang Rama Cipta Indah', city: 'Garut', address: 'C/25 Perum Rama Cipta Indah', hours: '10:00 - 20:00', mapUrl: 'https://maps.app.goo.gl/59XNibDEpx4ymcfa9' },
     { id: 3, name: 'Cabang BLK Garut', city: 'Garut', address: 'UPTD BLK Garut', hours: '10:00 - 20:00', mapUrl: 'https://maps.app.goo.gl/wPeJvLFoV85nTxhU8' },
     { id: 4, name: 'Cabang Mandala Residence', city: 'Garut', address: 'Blok M Mandala Residence 2', hours: '10:00 - 20:00', mapUrl: 'https://maps.app.goo.gl/V1xNmzNhmVojQ477A' },
-    { id: 5, name: 'Cabang ITS.Gen-Q', city: 'Garut', address: 'International Tahfizh School Generasi Qur\'an', hours: '10:00 - 20:00', mapUrl: 'https://maps.app.goo.gl/fjq3st5rjxifPadb9' },
+    { id: 5, name: 'Cabang ITS.Gen-Q', city: 'Garut', address: "International Tahfizh School Generasi Qur'an", hours: '10:00 - 20:00', mapUrl: 'https://maps.app.goo.gl/fjq3st5rjxifPadb9' },
   ];
 
   return (
@@ -28,6 +28,17 @@ const TokoPage = () => {
                     <strong>Alamat:</strong><br />
                     Jln A. Yani No. 302–302, Komplek Sukaregang Leather Center<br />
                     Kel. Kota Wetan, Kec. Garut Kota, Kab. Garut – Jawa Barat
+                  </div>
+                  {/* Link Google Maps toko pusat */}
+                  <div className="mt-3">
+                    <a
+                      href="https://maps.app.goo.gl/US2Z8yDnYfyZxnpr7"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="btn btn-primary"
+                    >
+                      Lihat di Google Maps
+                    </a>
                   </div>
                 </div>
                 <div>
